53daaa9c4332ac32fa55a9ba8085fdb05f46cae8
Yasser <saimyasser1@gmail.com>
2025-05-25 01:20:49 +0200

    commentPage added

---
+++ views/pages/commentPage/commentPage.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Commentaires</title>
+    <link rel="stylesheet" href="../../components/sideBar/sideBar.css">
+    <link rel="stylesheet" href="commentPage.css">
+</head>
+<body>
+    <div id="bar">
+        <?php include_once '../../components/sideBar/sideBar.php'; ?>
+    </div>
+    <div id="main">
+        <div id="comment">
+            <h1>Commentaires:</h1>
+            <div class="commentaire">
+                <p>Comment aire 1df qgrfz gfzgrza grarga fqezfag agragar gggrfrf efzegr</p>
+                <span>18/05/2025</span> 
+            </div>
+            <div class="commentaire">
+                <p>Commentaire 2</p>
+                <span>18/05/2025</span>
+            </div>
+            <div class="commentaire">
+                <p>Commentaire 3</p>
+                <span>18/05/2025</span>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
